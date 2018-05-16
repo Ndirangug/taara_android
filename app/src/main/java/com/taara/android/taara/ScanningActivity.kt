f2a5ef1eac79fc08b58770e1fec255027029b72d
George Ndirangu <ndirangu.mepawa@gmail.com>
2018-05-16 23:01:07 +0300

    initial commit

---
+++ app/src/main/java/com/taara/android/taara/ScanningActivity.kt
@@ -0,0 +1,12 @@
+package com.taara.android.taara
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ScanningActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanning)
+    }
+}
